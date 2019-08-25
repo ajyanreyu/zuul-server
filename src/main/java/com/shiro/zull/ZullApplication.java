0274ce70b2f9321944734c8be8d396087255ff3f
Albano Yanes <ajyanreyu@gmail.com>
2019-08-25 12:57:48 +0100

    Enable eureka and zull

--- src/main/java/com/shiro/zull/ZullApplication.java
+++ src/main/java/com/shiro/zull/ZullApplication.java
@@ -1,7 +1,13 @@
 package com.shiro.zull;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
 public class ZullApplication {
 
     public static void main(String[] args) {
