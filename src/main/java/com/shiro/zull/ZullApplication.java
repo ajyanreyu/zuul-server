4cca6ae487a260e20357861a49d6e942b375dc22
Albano Yanes <ajyanreyu@gmail.com>
2019-08-25 12:57:13 +0100

    Init zuul server microservice project

---
+++ src/main/java/com/shiro/zull/ZullApplication.java
@@ -0,0 +1,11 @@
+package com.shiro.zull;
+
+import org.springframework.boot.SpringApplication;
+
+public class ZullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZullApplication.class, args);
+    }
+
+}
